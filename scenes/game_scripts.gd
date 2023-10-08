0717a257ed2141cb1efbac936bb380c9f0a85fd4
leoomori <leoomorifaris@gmail.com>
2023-10-08 01:00:42 -0300

    add new ui styles

--- scenes/game_scripts.gd
+++ scenes/game_scripts.gd
@@ -1,9 +1,36 @@
 extends Node
 
+var cameraAnchor
+var camera
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	cameraAnchor = get_node("containerAreaViewPort/areaViewPort/arena/camera_anchor")
+	camera = get_node("containerAreaViewPort/areaViewPort/arena/camera_anchor/camera")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_up_pressed():
+	cameraAnchor.rotation.x = cameraAnchor.rotation.x - 0.1
+
+
+func _on_down_pressed():
+	cameraAnchor.rotation.x = cameraAnchor.rotation.x + 0.1
+
+
+func _on_left_pressed():
+	cameraAnchor.rotation.y = cameraAnchor.rotation.y - 0.1
+
+
+func _on_right_pressed():
+	cameraAnchor.rotation.y = cameraAnchor.rotation.y + 0.1
+
+func _on_zoom_out_pressed():
+	camera.position.y = camera.position.y + 5
+
+
+func _on_zoom_in_pressed():
+	camera.position.y = camera.position.y - 5
